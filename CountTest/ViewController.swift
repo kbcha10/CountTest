ffe79838609e7c99db8a36b55842a695b0ab8c20
kbcha10 <herbists.0v0@gmail.com>
2020-07-10 20:42:45 +0900

    発展課題解答

--- CountTest/ViewController.swift
+++ CountTest/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var pumpkinImage: UIImageView!
+    
+    let saveData: UserDefaults = UserDefaults.standard
+    
+    var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //UserDefaultsから値を取り出す
+        if (saveData.object(forKey: "number")) != nil {
+            number = saveData.object(forKey: "number") as! Int
+            label.text = String(number)
+        }
+        if (number % 5 == 0) {
+            pumpkinImage.image = UIImage(named: "pumpkin2")
+        } else {
+            pumpkinImage.image = UIImage(named: "pumpkin1")
+        }
     }
-
-
+    
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+        if (number % 5 == 0) {
+            pumpkinImage.image = UIImage(named: "pumpkin2")
+        } else {
+            pumpkinImage.image = UIImage(named: "pumpkin1")
+        }
+        //UserDefaultsに値を書き込む
+        saveData.set(number, forKey: "number")
+    }
 }
-
